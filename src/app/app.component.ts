d8a2e0cfcab9e833cd1d5eb384ad1afc9da1d279
Desangrador <017100864j@uandina.edu.pe>
2022-03-15 07:41:36 -0500

    vacio para revisar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,59 +35,5 @@ console.log("Productos y Almacenes vacios")
 mostrarProductos();
 mostrarAlmacenes();
 
-console.log("Crear Productos y Almacenes")
-
-//Crear Productos
-CrearProducto("agua", 1000)
-CrearProducto("gaseosa", 750)
-CrearProducto("pan", 650)
-CrearProducto("galleta",300)
-CrearProducto("pastel",450)
-
-//Crear Almacenes
-CrearAlmacen()
-CrearAlmacen()
-mostrarProductos();
-mostrarAlmacenes();
-
-console.log("Añadir productos a los almacenes")
-//Añadir productos a los almacenes
-//(producto, almacen, cantidad)
-//agua ->     1   almacen 1 ->  1
-//gaseosa ->  2   almacen 2 ->  2
-//pan ->      3
-//galleta ->  4
-//pastel ->   5
-
-añadir(5,1,450)
-añadir(3,2,200)
-mostrarAlmacenes();
-
-console.log("Modificar productos de los almacénes")
-//Modificar productos de los almacenes
-//(almacen, producto, cantidad)
-//agua ->     1   almacen 1 ->  1
-//gaseosa ->  2   almacen 2 ->  2
-//pan ->      3
-//galleta ->  4
-//pastel ->   5
-
-modificar(1,5,300)
-modificar(2,3,150)
-mostrarAlmacenes();
-
-console.log("Mover un producto de un almacen a otro")
-//Modificar productos de los almacenes
-//(almacenInicial, producto, cantidad, almacenFinal)
-//agua ->     1   almacen 1 ->  1
-//gaseosa ->  2   almacen 2 ->  2
-//pan ->      3
-//galleta ->  4
-//pastel ->   5
-mover(1,5,150,2)
-mostrarAlmacenes()
-mostrarProductos();
-mostrarDetalles();
-
 
 
